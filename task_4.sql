11517f7d9f27f6bf432f8da30531a421d5a98f99
Chuksugo <rhemekas.inc@gmail.com>
2024-10-22 15:32:17 +0100

    Full description + COLUMN TYPE

--- task_4.sql
+++ task_4.sql
@@ -1,8 +1,8 @@
 -- Select the alx_book_store database
 USE alx_book_store;
 
--- Query the INFORMATION_SCHEMA to get the full description of the 'books' table
-SELECT COLUMN_NAME, DATA_TYPE, CHARACTER_MAXIMUM_LENGTH, IS_NULLABLE, COLUMN_DEFAULT
+-- Query the INFORMATION_SCHEMA to get the full description of the 'Books' table
+SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_DEFAULT
 FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_NAME = 'books'
+WHERE TABLE_NAME = 'Books'
 AND TABLE_SCHEMA = 'alx_book_store';
